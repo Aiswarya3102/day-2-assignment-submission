f7bc1a4bfce13d19c6fe39040b79abeeaba644ee
Aiswarya3102 <92567537+Aiswarya3102@users.noreply.github.com>
2021-10-15 20:27:19 +0530

    Day 2 assignment submitted

---
+++ Day2/day 2.c
@@ -0,0 +1,22 @@
+int main( )
+{
+ int i,j,n;
+ printf("enter the value of n:");
+ scanf("%d",&n);
+ for(i=1;i<=n;i++)
+ {
+  for(j=1;j<=i;j++)
+  {
+   if((i%2==0&&j%2==1)||(i%2==1&&j%2==0))
+   {
+    printf("0");
+   }
+   else
+   {
+    printf("1");
+   }
+  }
+  printf("\n");
+ }
+ return 0;
+}
